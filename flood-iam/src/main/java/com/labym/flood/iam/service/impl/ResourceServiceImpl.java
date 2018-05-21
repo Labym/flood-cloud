cda02bb0774ebcd6a05a91d8ae20986b8a9942ec
zhangjingl02 <zhangjingl02@hotmail.com>
2018-05-21 21:06:49 +0800

    init

--- flood-iam/src/main/java/com/labym/flood/iam/service/impl/ResourceServiceImpl.java
+++ flood-iam/src/main/java/com/labym/flood/iam/service/impl/ResourceServiceImpl.java
@@ -1,8 +1,10 @@
 package com.labym.flood.iam.service.impl;
 
+import com.labym.flood.common.config.FloodDefaults;
 import com.labym.flood.common.constant.ResourceType;
 import com.labym.flood.common.exception.FloodErrorUtils;
 import com.labym.flood.common.exception.FloodException;
+import com.labym.flood.common.security.SecurityUtil;
 import com.labym.flood.common.service.impl.BaseServiceImpl;
 import com.labym.flood.common.util.tree.Tree;
 import com.labym.flood.iam.model.dto.ResourceDTO;
@@ -10,10 +12,13 @@ import com.labym.flood.iam.model.po.ResourcePO;
 import com.labym.flood.iam.repository.ResourceRepository;
 import com.labym.flood.iam.service.ResourceService;
 import com.labym.flood.iam.service.mapper.ResourceMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
+@Slf4j
 @Service
 public class ResourceServiceImpl extends BaseServiceImpl<ResourcePO, ResourceDTO, Long> implements ResourceService {
     private final ResourceRepository resourceRepository;
@@ -37,7 +42,10 @@ public class ResourceServiceImpl extends BaseServiceImpl<ResourcePO, ResourceDTO
         if (resourceRepository.findByName(resourceDTO.getName()).isPresent()) {
             throw new FloodException(FloodErrorUtils.alreadyExists("resource name already exist"));
         }
-
-
+        ResourcePO resourcePO = resourceMapper.toEntity(resourceDTO);
+        resourcePO.setCreateAt(Instant.now());
+        resourcePO.setCreateBy(SecurityUtil.currentUser());
+        resourceRepository.save(resourcePO);
+        log.info("created resource {}",resourcePO);
     }
 }
